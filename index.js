a1b454505e4710ac6273160edf4fd42ca7944f8c
Errorbot1122 <629329@lkstevens.wednet.edu>
2020-12-10 17:40:59 +0000

    Moved, Deleted and Added Some stuff in index.js
    Moved  to line 140
    
    Added  above
    
    Deleted Redundancies (like comments that had code in them that i don't need any more, extra chars, console logs, etc.)
    
    I Did some other Stuff to.

--- index.js
+++ index.js
@@ -136,12 +136,33 @@ function HandleError(req, res, errorcode) {
   res.type('txt').send(`Error ${errorcode}: Not found`);
 }
 
+/* Home */
+app.get('/', (req, res, next) => {
+  if (req.accepts('html') == "html") {
+    res.render('Home', {CodeEditorLink: req.protocol + '://' + req.get('host') + "/code-editor"})
+    
+    urlPath = req.protocol + '://' + req.get('host') + "/code-editor"
+    UrlSplit = urlPath.trim().split(/:?\/+/g);
+    return;
+
+  }
+
+  // respond with json
+  if (req.accepts('json') == "json") {
+    res.redirect("/api");
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send("linkes: " + req.protocol + '://' + req.get('host') + "/code-editor");
+}),
+
 app.get('/api', (req, res, next) => {
 
-});
+}),
 app.get('/api/roblox', (req, res, next) => {
 
-});
+}),
 app.get('/api/roblox/url/', async (req, res, next) => {
   urlPath = req.query.url
 
@@ -167,22 +188,8 @@ app.get('/api/roblox/url/', async (req, res, next) => {
   }
   
 }),
-/*
-app.get('/api/roblox/Url/', async (req, res, next) => {
-  //urlPath = req.query.url
-  
-  const UrlSplit = urlPath.toLowerCase().trim().split(/:?\/+/g);
-  const UrlBeging = UrlSplit.slice(0, 2);
-  
-  if (/(http)s?/.test(UrlBeging[0]) == true && /(roblox.com)/.test(UrlBeging[1])) {
-    const fetchRespose = await fetch(urlPath)
-    const fetchJson = await fetchRespose.json()
-    
-    res.send(fetchJson)
-  } else {
-    res.sendStatus(404);
-  }
-}),*/
+
+/* Code Editor */
 app.get('/code-editor', (req, res, next) => {
   if (req.accepts('html') == "html") {
     res.render('Editor', {})
@@ -201,25 +208,8 @@ app.get('/code-editor', (req, res, next) => {
   setTimeout(() => { res.redirect("/"); }, 3000);
 
 }),
-app.get('/', (req, res, next) => {
-  if (req.accepts('html') == "html") {
-    res.render('Home', {CodeEditorLink: req.protocol + '://' + req.get('host') + "/code-editor"})
-    
-    urlPath = req.protocol + '://' + req.get('host') + "/code-editor"
-    UrlSplit = urlPath.trim().split(/:?\/+/g);
-    return;
 
-  }
 
-  // respond with json
-  if (req.accepts('json') == "json") {
-    res.redirect("/api");
-    return;
-  }
-
-  // default to plain-text. send()
-  res.type('txt').send("linkes: " + req.protocol + '://' + req.get('host') + "/code-editor");
-}),
 /* Status Code Handling */ 
 app.get('/Error/:Errornumber', (req, res) => {
   HandleError(req, res, req.params.Errornumber)
@@ -230,19 +220,3 @@ app.all('*', (req, res) => {
 })
 
 app.listen(3000)
-
-/*
-const express = require("express");
-const app = express();
-
-
-app.get('/', (req, res) => {
-  res.send("lol");
-});
-
-app.all('*', (req, res, next) => {
-  
-});
-
-app.listen(3000);
-*/
